7172115d8f7e1b69358dfebb707fa0034e2076cf
catherinewang24 <catherinewang24@users.noreply.github.com>
2021-02-17 01:17:31 -0500

    Updated version
    Updated version as of Feb 17, 2021

---
+++ resources/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+/*
+    $('.js--scroll-to-concepts').click(function () {
+        $('html, body').animate({scrollTop: $('.js--concepts').offset().top + 100}, 1000); 
+     });
+*/
+});
